704660508397d99609c6383c4e2c13f15500f5d4
Ethan Shealey <ethan.shealey@gmail.com>
2022-01-09 16:05:31 -0500

    adding project

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const multer = require('multer')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+const upload = multer({ dest: 'uploads/' })
+
+app.use(cors({ optionSuccessStatus: 200 }))
+
+app.get('/', (req, res) => {
+  res.sendFile('./public/index.html', {root: __dirname})
+})
+
+app.post('/upload', upload.single('upfile'), (req, res) => {
+  console.log(req.file)
+  return res.json({
+    'name': req.file.originalname,
+    'type': req.file.mimetype,
+    'size': req.file.size
+  })
+})
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`listening on port ${process.env.PORT || 3001}`)
+})
